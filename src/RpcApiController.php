38acf6f2143c16e00b904ee8f47d2633192c2f90
Webcitron - Adam Mackiewicz <a.mackiewicz@webcitron.eu>
2015-04-30 18:19:13 +0200

    x

--- src/RpcApiController.php
+++ src/RpcApiController.php
@@ -15,30 +15,18 @@ class RpcApiController {
     }
     
     public function fireMethod ($strMethodPointer, $arrParams) {
-        $arrReturn = array();
+        $arrResponse = array();
         $objApplicationContext = \webcitron\Subframe\Application::getInstance();
         require APP_DIR.'/'.$objApplicationContext->strName.'/config/rpcapi.php';
-//        exit();
-//        echo '<pre>';
-//        var_dump($strMethodPointer);
-//        var_dump($this->arrAllowedMethods);
-//        echo '</pre>';
-//        exit();
-        if (in_array($strMethodPointer, $this->arrAllowedMethods)) {
-//            echo 'not in array'; exit();
-//            $arrReturn['error'] = 'security error';
-//        } else {
-//            echo 'in array'; exit();
+        
+        if (!in_array($strMethodPointer, $this->arrAllowedMethods)) {
+            $objResponse['error'] = 'that rpc method is not allowed do use remotely';
+        } else {
             $arrMethodPointerTokens = explode('.', $strMethodPointer);
             $strMethodName = array_pop($arrMethodPointerTokens);
             $strClassFullPath = sprintf('\\backend\\%s', join('\\', $arrMethodPointerTokens));
-//            echo $strClassFullPath.'::'.$strMethodName;
-//            exit();
-//            $objSpecifiedBoard = new $strBoardFullPath();
             $objBoardMethod = new \ReflectionMethod($strClassFullPath, $strMethodName);
-//            $arrRequestParams = Request::getParams();
-            $objResponse = $objBoardMethod->invokeArgs(null, $arrParams);
-            
+            $objResponse = $objBoardMethod->invokeArgs(null, $arrParams);   
         }
         return $objResponse;
     }
