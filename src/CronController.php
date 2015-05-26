740ba22f00070a21095c9bccdc542a3cab908160
Webcitron - Adam Mackiewicz <a.mackiewicz@webcitron.eu>
2015-05-26 10:30:57 +0200

    x

--- src/CronController.php
+++ src/CronController.php
@@ -6,30 +6,20 @@ class CronController {
     
     
     public function fireMethod ($strMethodPointer, $arrParams) {
-        $arrResponse = array();
-        $objApplicationContext = \webcitron\Subframe\Application::getInstance();
-//        echo APP_DIR.'/'.$objApplicationContext->strName.'/config/rpcapi.php';
-//        exit();
-//        require APP_DIR.'/'.$objApplicationContext->strName.'/config/rpcapi.php';
-//        
-//        if (!in_array($strMethodPointer, $this->arrAllowedMethods)) {
-//            $objResponse['error'] = 'that rpc method is not allowed do use remotely';
-//        } else {
+//        $arrResponse = array();
+//        $objApplicationContext = \webcitron\Subframe\Application::getInstance();
             $arrMethodPointerTokens = explode('.', $strMethodPointer);
             $strMethodName = array_pop($arrMethodPointerTokens);
             $strClassFullPath = sprintf('\\backend\\%s', join('\\', $arrMethodPointerTokens));
             
-            echo $strClassFullPath.'::'.$strMethodName;
+//            echo $strClassFullPath.'::'.$strMethodName;
 //            
-//            echo '<pre>';
-//            print_r($arrParams);
-//            exit();
             $objMethod = new \ReflectionMethod($strClassFullPath, $strMethodName);
             $objResponse = $objMethod->invokeArgs(null, $arrParams);   
-            echo 'done';
-            echo '<Pre>';
-            print_r($objResponse);
-            exit();
+//            echo 'done';
+//            echo '<Pre>';
+//            print_r($objResponse);
+//            exit();
 //        }
         return $objResponse;
     }
