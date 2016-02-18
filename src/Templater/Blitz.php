dd0daa48452f23218ec8e90e52aec9645554a1c5
Adam Mackiewicz / WebCitron <a.mackiewicz@webcitron.eu>
2016-02-18 11:01:12 +0100

    x

--- src/Templater/Blitz.php
+++ src/Templater/Blitz.php
@@ -267,7 +267,7 @@ class SubBlitz extends \Blitz implements \webcitron\Subframe\ITemplaterHelper {
                 $strCellClasses = $arrConfig[$numRowConfigIndex][1][$numItemInRowIndex];
                 $arrItem['strAlt'] = strip_tags(trim($arrItem['title'], '. ').'. '.trim($arrItem['description']));
                 if (strlen($arrItem['strAlt']) > 200) {
-                    $arrItem['strAlt'] = substr($arrItem['strAlt'], 0, 200).'...';
+                    $arrItem['strAlt'] = mb_substr($arrItem['strAlt'], 0, 200, 'UTF-8').'...';
                 }
                 $strCell = $this->include($strTempalatePath, $arrItem);
                 $strHtml .= '<div class="item-wrapper '.$strCellClasses.'">'.$strCell.'</div>';
