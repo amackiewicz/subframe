22a5c734e748b0d0cedf8547cdb9cbe724d0f117
Adam Mackiewicz <a.mackiewicz@webcitron.eu>
2017-11-23 13:44:10 +0100

    #2554

--- src/CssController.php
+++ src/CssController.php
@@ -1,63 +1,68 @@
 <?php namespace webcitron\Subframe;
 
 
-class CssController {
-    
+class CssController
+{
+
     public static $objInstance = null;
     public $arrStylesheetsToLoad = array();
     public $strForceCssFile = '';
     private $strCssHost = 'https://static.imged.pl';
-    
+
     private $numDeployVersion = 0;
-    
-    public static function getInstance () {
+
+    public static function getInstance()
+    {
         if (self::$objInstance === null) {
             self::$objInstance = new CssController();
         }
         return self::$objInstance;
     }
-    
-    public function forceCssFile ($strFile) {
+
+    public function forceCssFile($strFile)
+    {
         $this->strForceCssFile = $strFile;
     }
-    
-    private function __construct () {
-        $numDeployVersionNumber = file_get_contents(APP_DIR.'/../deploy-version.php');
+
+    private function __construct()
+    {
+        $numDeployVersionNumber = file_get_contents(APP_DIR . '/../deploy-version.php');
         if (!empty($numDeployVersionNumber)) {
             $this->numDeployVersion = trim($numDeployVersionNumber);
         }
     }
-    
-    public function renderAsync ($strApplicationName) {
+
+    public function renderAsync($strApplicationName)
+    {
         if (!empty($this->strForceCssFile)) {
             $strCssFile = $this->strForceCssFile;
         } else {
             $objRouter = Router::getInstance();
             $objCurrentRoute = $objRouter->getCurrentRoute();
-            $strCssFile = $objCurrentRoute->strRouteName.'_'.$objCurrentRoute->strMethodName;
+            $strCssFile = $objCurrentRoute->strRouteName . '_' . $objCurrentRoute->strMethodName;
         }
         $strCssHhtml = sprintf("<script>\nvar arrSubframeCssToLoad = ['/%s/css/%s.css'];\n</script>", $strApplicationName, $strCssFile);
-        
+
         return $strCssHhtml;
     }
-    
-    public function render ($strApplicationName) {
+
+    public function render($strApplicationName)
+    {
         $strCssFullPath = '';
         if (!empty($this->strForceCssFile)) {
             $strCssFile = $this->strForceCssFile;
         } else {
             $objRouter = Router::getInstance();
             $objCurrentRoute = $objRouter->getCurrentRoute();
-            $strCssFile = $objCurrentRoute->strRouteName.'_'.$objCurrentRoute->strMethodName;
+            $strCssFile = $objCurrentRoute->strRouteName . '_' . $objCurrentRoute->strMethodName;
         }
         $numEnvironment = Application::currentEnvironment();
 
         if ($numEnvironment === Application::ENVIRONMENT_DEV) {
-            $strApplicationBaseUrl = \webcitron\Subframe\Application::url(); 
-            $strCssFullPath = sprintf('%s/%s/css/%s.css', 
-                $strApplicationBaseUrl, 
-                $strApplicationName, 
-                // $this->numDeployVersion, 
+            $strApplicationBaseUrl = \webcitron\Subframe\Application::url();
+            $strCssFullPath = sprintf('%s/%s/css/%s.css',
+                $strApplicationBaseUrl,
+                $strApplicationName,
                 $strCssFile
             );
 
@@ -66,40 +71,41 @@ class CssController {
             if ($numEnvironment === Application::ENVIRONMENT_RC) {
                 $strCssPrefix .= '/rc';
             }
-            $strCssFullPath = sprintf('%s/assets/v%s/css/%s.css', 
-                $strCssPrefix, 
-                $this->numDeployVersion, 
+            $strCssFullPath = sprintf('%s/assets/v%s/%s/css/%s.css',
+                $strCssPrefix,
+                $this->numDeployVersion,
+                $strApplicationName,
                 $strCssFile
             );
         }
 
         $strCssHhtml = '';
         if (!empty($strCssFullPath)) {
-            $strCssHhtml = '<link rel="stylesheet" href="'.$strCssFullPath.'" />';
+            $strCssHhtml = '<link rel="stylesheet" href="' . $strCssFullPath . '" />';
         }
-        
 
 
-            // if ($numEnvironment === Application::ENVIRONMENT_PRODUCTION) {
-            //     $strApplicationBaseUrl = 'https://'.\webcitron\Subframe\Application::url(false);
-            // } else {
-            //     $strApplicationBaseUrl = \webcitron\Subframe\Application::url();    
-            // }
-            
-            // echo $strApplicationBaseUrl;
-            // exit();
-            // $strCssHhtml = sprintf('<link rel="stylesheet" href="%s/%s/css/cacheversion-%d/%s.css" />', $strApplicationBaseUrl, $strApplicationName, $this->numDeployVersion, $strCssFile);
-            //$strCssHhtml = sprintf('<link rel="stylesheet" href="%s/rc/assets/css/v%d/%s.css" />', $this->strCssHost, $this->numDeployVersion, $strCssFile);
-        
+        // if ($numEnvironment === Application::ENVIRONMENT_PRODUCTION) {
+        //     $strApplicationBaseUrl = 'https://'.\webcitron\Subframe\Application::url(false);
+        // } else {
+        //     $strApplicationBaseUrl = \webcitron\Subframe\Application::url();
+        // }
+
+        // echo $strApplicationBaseUrl;
+        // exit();
+        // $strCssHhtml = sprintf('<link rel="stylesheet" href="%s/%s/css/cacheversion-%d/%s.css" />', $strApplicationBaseUrl, $strApplicationName, $this->numDeployVersion, $strCssFile);
+        //$strCssHhtml = sprintf('<link rel="stylesheet" href="%s/rc/assets/css/v%d/%s.css" />', $this->strCssHost, $this->numDeployVersion, $strCssFile);
+
         return $strCssHhtml;
     }
-    
-    public static function addStylesheets ($arrCssFiles) {
+
+    public static function addStylesheets($arrCssFiles)
+    {
         $objCssController = self::getInstance();
         if (!is_array($arrCssFiles)) {
             $arrCssFiles = array($arrCssFiles);
         }
         $objCssController->arrStylesheetsToLoad = array_unique(array_merge($objCssController->arrStylesheetsToLoad, $arrCssFiles));
     }
-    
+
 }
--- src/JsController.php
+++ src/JsController.php
@@ -2,30 +2,34 @@
 
 use webcitron\Subframe\Languages;
 
-class JsController {
-    
+class JsController
+{
+
     public static $objInstance = null;
     public $boolRunJs = false;
     public $arrScriptsToLoad = array();
     // public $arrCustomToLoad = array();
     private $numDeployVersion = 0;
     private $strJsHost = 'https://static.imged.pl';
-    
-    public static function getInstance () {
+
+    public static function getInstance()
+    {
         if (self::$objInstance === null) {
             self::$objInstance = new JsController();
         }
         return self::$objInstance;
     }
-    
-    private function __construct () {
-        $numDeployVersionNumber = file_get_contents(APP_DIR.'/../deploy-version.php');
+
+    private function __construct()
+    {
+        $numDeployVersionNumber = file_get_contents(APP_DIR . '/../deploy-version.php');
         if (!empty($numDeployVersionNumber)) {
             $this->numDeployVersion = trim($numDeployVersionNumber);
         }
     }
-    
-    public function render ($strApplicationName, $numEnableCaching = 1) {
+
+    public function render($strApplicationName, $numEnableCaching = 1)
+    {
         if ($this->boolRunJs !== true) {
             return;
         }
@@ -33,80 +37,79 @@ class JsController {
 
         $numEnvironment = Application::currentEnvironment();
         $objApp = Application::getInstance();
-        
+
         if ($numEnvironment === Application::ENVIRONMENT_PRODUCTION && $objApp->strName !== 'admin') {
-            $strApplicationBaseUrl = 'https://'.\webcitron\Subframe\Application::url(false);
+            $strApplicationBaseUrl = 'https://' . \webcitron\Subframe\Application::url(false);
         } else {
             $strApplicationBaseUrl = \webcitron\Subframe\Application::url();
         }
 
-        
+
         // echo $strApplicationBaseUrl;
         // exit();
         $objApp = \webcitron\Subframe\Application::getInstance();
 
         $objLanguages = Languages::getInstance();
         $strCurrentLanguage = $objLanguages->getCurrentLanguage();
-        
-        $strPostfixCache = ''; 
+
+        $strPostfixCache = '';
         if ($numEnableCaching === 0) {
-            $strPostfixCache = '&_='.time();
+            $strPostfixCache = '&_=' . time();
         }
-         
+
         $strLaunchCode = '';
-        $strLaunchCode .= '<script>'.PHP_EOL;
-        $strLaunchCode .= 'var boolIsPuppiesBlocked = true;'.PHP_EOL; 
+        $strLaunchCode .= '<script>' . PHP_EOL;
+        $strLaunchCode .= 'var boolIsPuppiesBlocked = true;' . PHP_EOL;
         // $strLaunchCode .= 'var d = new Date();var numJsPointTimestamp = d.getTime();'.PHP_EOL;
-        $strLaunchCode .= '</script>'.PHP_EOL;
+        $strLaunchCode .= '</script>' . PHP_EOL;
 
         $objRoute = Router::getCurrentRoute();
         $strBoardJsFilename = str_replace('::', '_', $objRoute->strRouteFullName);
 
         if ($numEnvironment === Application::ENVIRONMENT_DEV) {
-            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/subframe/js/adblock-advertisement.js"></script>', $strApplicationBaseUrl).PHP_EOL;
-            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/%s/js/board_min/%s.min.js"></script>', $strApplicationBaseUrl, $objApp->strName, $strBoardJsFilename).PHP_EOL;
+            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/subframe/js/adblock-advertisement.js"></script>', $strApplicationBaseUrl) . PHP_EOL;
+            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/%s/js/board_min/%s.min.js"></script>', $strApplicationBaseUrl, $objApp->strName, $strBoardJsFilename) . PHP_EOL;
         } else {
             $strJsPrefix = $this->strJsHost;
             if ($numEnvironment === Application::ENVIRONMENT_RC) {
                 $strJsPrefix .= '/rc';
             }
-            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/assets/adblock-advertisement.js"></script>', 
+            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/assets/adblock-advertisement.js"></script>',
                 $strJsPrefix
             );
-            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/assets/v%s/js/%s.min.js"></script>', 
-                $strJsPrefix, 
-                $this->numDeployVersion, 
+            $strLaunchCode .= sprintf('<script type="text/javascript" src="%s/assets/v%s/%s/js/%s.min.js"></script>',
+                $strJsPrefix,
+                $this->numDeployVersion,
+                $strApplicationName,
                 $strBoardJsFilename
             );
         }
 
-        
-        
-        
-        
-        $strLaunchCode .= '<script>'.PHP_EOL; 
-        
-        $strLaunchCode .= sprintf('var objLauncher = new Subframe.Lib.Launcher("%s", "%s", "%s", "%s", %s, ["%s"]);', $strApplicationName, $strApplicationBaseUrl, $strCurrentLanguage, $this->numDeployVersion, $numEnableCaching, join('", "', $this->arrScriptsToLoad)).PHP_EOL;
-        $strLaunchCode .= 'objLauncher.init();'.PHP_EOL;
-        $strLaunchCode .= '</script>'.PHP_EOL;
+
+        $strLaunchCode .= '<script>' . PHP_EOL;
+
+        $strLaunchCode .= sprintf('var objLauncher = new Subframe.Lib.Launcher("%s", "%s", "%s", "%s", %s, ["%s"]);', $strApplicationName, $strApplicationBaseUrl, $strCurrentLanguage, $this->numDeployVersion, $numEnableCaching, join('", "', $this->arrScriptsToLoad)) . PHP_EOL;
+        $strLaunchCode .= 'objLauncher.init();' . PHP_EOL;
+        $strLaunchCode .= '</script>' . PHP_EOL;
         return $strLaunchCode;
     }
-    
+
     // public static function addCustomJs ($strFile) {
     //     $objJsController = self::getInstance();
     //     $objJsController->boolRunJs = true;
     //     $objJsController->arrCustomToLoad[] = $strFile;
     // }
-    
-    public static function runJs () {
+
+    public static function runJs()
+    {
         $objJsController = self::getInstance();
         $objJsController->boolRunJs = true;
         $arrBacktrace = debug_backtrace();
         $arrContext = $arrBacktrace[1];
         if (!empty($arrContext['class'])) {
-            $strJsFilePath = str_replace('\\', '/', substr($arrContext['class'], strpos($arrContext['class'], 'box\\')+4));
+            $strJsFilePath = str_replace('\\', '/', substr($arrContext['class'], strpos($arrContext['class'], 'box\\') + 4));
             $objJsController->arrScriptsToLoad[] = $strJsFilePath;
         }
     }
-    
+
 }
